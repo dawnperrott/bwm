eada18fdfad5e4f9f2c41c107a64d899c0826916
Dawn Perrott <100697701+dawnperrott@users.noreply.github.com>
2022-11-25 16:10:14 +0000

    sixth commit
    added search bar , updated homepage

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,5 +1,12 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS beer;
+DROP TABLE IF EXISTS pub;
+DROP TABLE IF EXISTS treaty_city;
+DROP TABLE IF EXISTS sextons;
+DROP TABLE IF EXISTS the_commercial;
+DROP TABLE IF EXISTS crew;
+DROP TABLE IF EXISTS the_white_house;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,3 +22,76 @@ CREATE TABLE post (
   body TEXT NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
+
+CREATE TABLE beer (
+  beer_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  beer_name TEXT NOT NULL,
+  beer_description TEXT NOT NULL
+);
+
+CREATE TABLE pub (
+  pub_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  pub_name TEXT NOT NULL,
+  location TEXT NOT NULL,
+  pub_description TEXT NOT NULL
+);
+
+CREATE TABLE treaty_city (
+  pub_id INTEGER NOT NULL,
+  beer_id INTEGER NOT NULL,
+  beer_name TEXT NOT NULL,
+  FOREIGN KEY (pub_id) REFERENCES pub (pub_id),
+  FOREIGN KEY (beer_id) REFERENCES beer (beer_id)
+);
+
+CREATE TABLE sextons (
+  pub_id INTEGER NOT NULL,
+  beer_id INTEGER NOT NULL,
+  beer_name TEXT NOT NULL,
+  FOREIGN KEY (pub_id) REFERENCES pub (pub_id),
+  FOREIGN KEY (beer_id) REFERENCES beer (beer_id)
+);
+
+CREATE TABLE the_commercial (
+  pub_id INTEGER NOT NULL,
+  beer_id INTEGER NOT NULL,
+  beer_name TEXT NOT NULL,
+  FOREIGN KEY (pub_id) REFERENCES pub (pub_id),
+  FOREIGN KEY (beer_id) REFERENCES beer (beer_id)
+);
+
+CREATE TABLE crew (
+  pub_id INTEGER NOT NULL,
+  beer_id INTEGER NOT NULL,
+  beer_name TEXT NOT NULL,
+  FOREIGN KEY (pub_id) REFERENCES pub (pub_id),
+  FOREIGN KEY (beer_id) REFERENCES beer (beer_id)
+);
+
+CREATE TABLE the_white_house (
+  pub_id INTEGER NOT NULL,
+  beer_id INTEGER NOT NULL,
+  beer_name TEXT NOT NULL,
+  FOREIGN KEY (pub_id) REFERENCES pub (pub_id),
+  FOREIGN KEY (beer_id) REFERENCES beer (beer_id)
+);
+
+INSERT INTO beer (beer_name, beer_description)
+VALUES ('Ecstatic citrus','Malty & Sweet, 5% alcohol'),
+       ('Dark Drizzle','Dark & Roasty, 4.5% alcohol'),
+       ('Curious Drop','Sour, Tart, & Funky, 3.5% alcohol'),
+       ('Mosaic Dry Hopped','Hoppy & Bitter, 5.5% alcohol'),
+       ('Double Foggy','Smokey, 4.75% alcohol'),
+       ('Enchanted Hallucination','Crisp & Clean, 3.5% alcohol'),
+       ('Lucid Daydream','Fruit & Spice, 5% alcohol'),
+       ('Murk Wisp','Light, delicate, crisp and refreshing., 5% alcohol');
+
+INSERT INTO pub (pub_name, location, pub_description )
+VALUES ('Treaty City','Medieval Quarter, 24 & 25 Nicholas St, Limerick, V94 EH57','Houses a large selection of ales to choose from, Cozy and warm'),
+       ('Sextons',' 91 Henry St, Limerick, V94 CA29',' Large open space with outdoor pizza oven'),
+       ('The Commercial','46-47 Catherine St, Limerick, V94 T2V3','Retro bar, previously a Hotel'),
+       ('Crew Brewery','35 Thomas St, Limerick, V94 A5X4', 'Locally produces craft beers on site'),
+       ('The White House','52 O,Connell St, Limerick, V94 NYH9','Traditional music played regularly');
+
+
+
--- flaskr/searchbar.py
+++ flaskr/searchbar.py
@@ -1,45 +1,43 @@
 from flask import Flask, Blueprint, render_template, request, make_response, url_for
+
 import sqlite3
+
 from werkzeug.exceptions import abort
 
-import db
-from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('searchbar', __name__)
 
-# @bp.route('/searches')  # , methods=['GET', 'POST'])
-# @login_required
-# def searches():
-# form = SearchForm()
-# if request.method == 'POST' and form.validate_on_submit():
-# return redirect((url_for('search_results', query=form.search.data)))
-#    return render_template('searchbar/searches.html')
 
-
-#
-#def getbeers(search):
-#    conn = sqlite3.connect(dbFILE)
-#    cursor = conn.cursor()
-#    cursor.execute(
-#       "SELECT * FROM `user` WHERE `name` LIKE ? OR `password` LIKE ?",
-#        ("%" + search + "%", "%" + search + "%",)
-#    )
-#    results = cursor.fetchall()
-#    conn.close()
-#    return results
-
-
-#
-@bp.route("/searches", methods=["GET", "POST"])
+@bp.route("/searches", methods=('GET', 'POST'))
 def searches():
-    #  SEARCH FOR beers
- #    if request.method == "POST":
- #       data = dict(request.form)
-  #      user = getbeers(data["search"])
-  #  else:
-  #      user = []
+    pub = ""
+    if request.method == 'POST':
+        searched = request.form['searched']
+        db = get_db()
+        error = None
 
-    return render_template("searchbar/searches.html")  #, usr=user)
+        if not searched:
+            error = 'Search is required.'
 
+        pub = db.execute(
+            'SELECT * FROM pub WHERE pub_name LIKE ?', (searched,)
+        ).fetchall()
+        if pub:
+            print(pub[0][0])
+            print(pub[0][1])
+            print(pub[0][2])
+            print(pub[0][3])
+        else:
+            beer = db.execute(
+                'SELECT * FROM beer WHERE beer_name LIKE ?', (searched,)
+            ).fetchall()
+            if beer:
+                print(beer[0][0])
+                print(beer[0][1])
+                print(beer[0][2])
+            pub = beer
 
+        db.close()
+
+    return render_template("searchbar/searches.html", rows=pub)
